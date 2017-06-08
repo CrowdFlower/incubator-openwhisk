43ab7521e01c19eadddcd162fdb9f1e7846fafa5
Jesse Washburn <jesse@crowdflower.com>
2017-06-08 10:15:14 -0400

    Set the docker API systemd-style; allow localhost queries only.
    My original CentOS setup specified these options in /etc/default/docker, but they were not being honored because provisioning (`ansible-playbook openwhisk.yml`) failed at the step where it tried to communicate with the local docker API.

--- tools/centos-setup/docker.sh
+++ tools/centos-setup/docker.sh
@@ -16,14 +16,16 @@ sudo yum makecache fast
 sudo yum install -y docker-ce.x86_64
 
 # enable (security - use 127.0.0.1)
-sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=aufs"'\'' >> /etc/default/docker'
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo -E bash -c 'echo -e '\''[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H tcp://127.0.0.1:4243 -H unix:///var/run/docker.sock'\'' > /etc/systemd/system/docker.service.d/remote-api.conf'
 sudo -E bash -c 'echo -e '\''{\n  "storage-driver": "devicemapper"\n}'\'' > /etc/docker/daemon.json'
 sudo gpasswd -a `whoami` docker
 
 # Set DOCKER_HOST as an environment variable
-sudo -E bash -c 'echo '\''export DOCKER_HOST="tcp://0.0.0.0:4243"'\'' >> /etc/bash.bashrc'
+sudo -E bash -c 'echo '\''export DOCKER_HOST="tcp://127.0.0.1:4243"'\'' >> /etc/bash.bashrc'
 source /etc/bash.bashrc
 
+sudo systemctl daemon-reload
 sudo systemctl enable docker.service
 sudo systemctl restart docker.service
 
